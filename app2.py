118f16ab24d4a7d90aef48218d265daf4e66ad60
VedranaGATech <vedranab79@gmail.com>
2022-08-22 18:25:04 -0500

    testing file removed

--- app2.py
+++ app2.py
@@ -1,23 +1,33 @@
 import numpy as np
+import pandas as pd
+import datetime as dt
+import flask
+from flask import jsonify
+
+# Python SQL toolkit and Object Relational Mapper
+import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
-from sqlalchemy import create_engine, func
-from flask import Flask, jsonify
-
-#################################################
-# Database Setup
-#################################################
+from sqlalchemy import create_engine, func, inspect
+# create engine to hawaii.sqlite
 engine = create_engine("sqlite:///Resources/hawaii.sqlite")
-
 
 # reflect an existing database into a new model
 Base = automap_base()
 # reflect the tables
 Base.prepare(engine, reflect=True)
 
+Base.classes.keys()
+
 # Save references to each table
 Measurement = Base.classes.measurement
 Station = Base.classes.station
+
+# Create our session (link) from Python to the DB
+session = Session(engine)
+
+# Inspect
+inspector = inspect(engine)
 
 #################################################
 # Flask Setup
